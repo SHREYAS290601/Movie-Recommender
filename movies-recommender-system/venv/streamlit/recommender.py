498a9bcf70f37fd4792af5715584b66e65c025e5
Shreyas Kulkarni <shreyas290601@gmail.com>
2022-11-21 23:51:14 +0530

    Minor changes in Streamlit

--- movies-recommender-system/venv/streamlit/recommender.py
+++ movies-recommender-system/venv/streamlit/recommender.py
@@ -29,11 +29,11 @@ st.markdown(
     .css-14xya8e img {
         max-width:20%
     }
+    
     </style>
     """,
     unsafe_allow_html=True
 )
-
 
 
 SimilarityScore = pickle.load(open('../../Similarity.pkl', 'rb'))
@@ -44,11 +44,12 @@ df = pandas.DataFrame(df)
 df_values_title = df.title.values
 
 st.title('Movie Recommender System')
-st.markdown("<center style=\"font-size:24px;\">This is a Movie recommendation system developed by Shreyas Kulkarni and Rohan Rasne of BEcomp</center>",unsafe_allow_html=True)
-st.markdown("<br>",unsafe_allow_html=True)
-st.markdown("<br>",unsafe_allow_html=True)
-st.markdown("<br>",unsafe_allow_html=True)
-st.markdown("<p>Set of Instructions to be followed</p>",unsafe_allow_html=True)
+st.markdown("<center style=\"font-size:24px;\">This is a Movie recommendation system developed by Shreyas Kulkarni and Rohan Rasne of BEcomp</center>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<p>Set of Instructions to be followed</p>",
+            unsafe_allow_html=True)
 st.markdown("""
             <ul style=\"color:#14C38E"\>
                 <li>The Recommendation is based on Vector Space model which employees the Cosine Similarity
@@ -57,32 +58,35 @@ st.markdown("""
                 <li>The recommendations for the current movie will pop up after sometime(3-4sec)
                 <li>Then based on your movie knowledge compare if they are actually true!!
             </ul>
-            """            ,unsafe_allow_html=True)
+            """, unsafe_allow_html=True)
 st.sidebar.success("Search here!")
-selected_movie = st.sidebar.selectbox('Type or select a movie from the dropdown', df_values_title)
+selected_movie = st.sidebar.selectbox(
+    'Type or select a movie from the dropdown', df_values_title)
 
 
 def recommender(movie_name):
     movie_index = df[df.title == movie_name].index[0]
-    movie_top_10 = sorted(list(enumerate(SimilarityScore[movie_index])), reverse=True, key=lambda x: x[1])[1:10]
+    movie_top_10 = sorted(list(enumerate(
+        SimilarityScore[movie_index])), reverse=True, key=lambda x: x[1])[1:10]
     recommended = []
     recommend_id = []
-    recommend_poster=[]
-    rec_des=[]
-    rec_gen=[]
+    recommend_poster = []
+    rec_des = []
+    rec_gen = []
 
     for i in movie_top_10:
-        movie_id=df.iloc[i[0]].movie_id
+        movie_id = df.iloc[i[0]].movie_id
         recommended.append(df.iloc[i[0]].title)
         recommend_id.append(df.iloc[i[0]].movie_id)
         recommend_poster.append(fetch_data_poster(movie_id))
         rec_des.append(fetch_data_describe(movie_id))
         rec_gen.append(fetch_data_genre(movie_id))
-    return recommended, recommend_poster,rec_des,rec_gen
+    return recommended, recommend_poster, rec_des, rec_gen
+
 
 def fetch_data_poster(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key='+key+'&language=en-US'.format(
+        'https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(
             movie_id))
     data = response.json()
 
@@ -90,17 +94,19 @@ def fetch_data_poster(movie_id):
 
     return poster_path
 
+
 def fetch_data_describe(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key='+key+'&language=en-US'.format(
+        'https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(
             movie_id))
     data = response.json()
-    describe =  data['overview']
+    describe = data['overview']
     return describe
-    
+
+
 def fetch_data_genre(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=9'+key+'&language=en-US'.format(
+        'https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(
             movie_id))
     data = response.json()
     genres = data['genres']
@@ -108,24 +114,28 @@ def fetch_data_genre(movie_id):
 
 
 if st.sidebar.button('Recommend'):
-    rec=recommender(selected_movie)
+    rec = recommender(selected_movie)
     # st.write(rec)
-    recommendations,posters,describe,genres= recommender(selected_movie)
-    print(describe[0],genres[0][0]['name'])
+    movie_index = df[df.title == selected_movie].index[0]
+    st.sidebar.image(fetch_data_poster(df.loc[movie_index].movie_id))
+    st.sidebar.write(fetch_data_genre(df.loc[movie_index].movie_id)[0]['name'])
+    st.sidebar.write(fetch_data_describe(df.loc[movie_index].movie_id))
+    # st.image()
+    recommendations, posters, describe, genres = recommender(selected_movie)
+    # print(describe[0], genres[0][0]['name'])
 
-    col1, col2 = st.columns([1,1])
-    
-   
-    for i in range(0,9):
-        if i%2==0:
-            col1.write(recommendations[i])
+    col1, col2 = st.columns([1, 1])
+
+    for i in range(0, 9):
+        if i % 2 == 0:
+            col1.markdown("<p style=\"font-size:28px\">" +
+                          recommendations[i]+"</p>", unsafe_allow_html=True)
             col1.image(posters[i])
-            col1.write('{}...'.format(describe[i])[:300])
             col1.write("Genre:{}".format(genres[i][0]['name']))
+            col1.write('{}...'.format(describe[i])[:300])
         else:
-            col2.write(recommendations[i])
+            col2.markdown("<p style=\"font-size:28px\">" +
+                          recommendations[i]+"</p>", unsafe_allow_html=True)
             col2.image(posters[i])
-            col2.write('{}'.format(describe[i])[:300])
             col2.write("Genre:{}".format(genres[i][0]['name']))
-
-    
+            col2.write('{}'.format(describe[i])[:300])
